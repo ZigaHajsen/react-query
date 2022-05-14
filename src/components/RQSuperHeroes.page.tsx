2aebfe83334c068077a22b3e9e63ae2e3a414a02
Žiga Hajšen <ziga.hajsen@gmail.com>
2022-05-14 17:50:17 +0200

    react query error handling

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,17 +1,24 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import { useQuery } from 'react-query';
 
 import { Hero } from '../types';
 
 const fetchSuperHeroes = () => {
-  return axios.get<Hero[]>('http://localhost:4000/superheroes');
+  return axios.get<Hero[]>('http://localhost:4000/superheroes1');
 };
 
 export const RQSuperHeroesPage: React.FC = () => {
-  const { data, isLoading } = useQuery('super-heroes', fetchSuperHeroes);
+  const { data, isLoading, isError, error } = useQuery<
+    AxiosResponse<Hero[]>,
+    AxiosError
+  >('super-heroes', fetchSuperHeroes);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
 
   return (
