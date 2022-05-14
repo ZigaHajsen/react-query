30d5c67f95fac63b20054e49036bc85003cac823
Žiga Hajšen <ziga.hajsen@gmail.com>
2022-05-14 17:50:04 +0200

    traditional error handling

--- src/components/SuperHeroes.page.tsx
+++ src/components/SuperHeroes.page.tsx
@@ -4,17 +4,29 @@ import { Hero } from '../types';
 
 export const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState<Hero[]>([]);
+  const [data, dataSet] = useState<Hero[]>([]);
+  const [error, errorSet] = useState('');
 
   useEffect(() => {
-    axios.get<Hero[]>('http://localhost:4000/superheroes').then((res) => {
-      setData(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get<Hero[]>('http://localhost:4000/superheroes')
+      .then((res) => {
+        dataSet(res.data);
+      })
+      .catch((error) => {
+        errorSet(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>{error}</h2>;
   }
 
   return (
