4c53ab621b5490e4d36769730a11a75a99ab90d8
Žiga Hajšen <ziga.hajsen@gmail.com>
2022-05-14 17:42:46 +0200

    added useQuery hook

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,3 +1,25 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+import { Hero } from '../types';
+
+const fetchSuperHeroes = () => {
+  return axios.get<Hero[]>('http://localhost:4000/superheroes');
+};
+
 export const RQSuperHeroesPage: React.FC = () => {
-  return <div>RQ Super Heroes Page</div>;
+  const { data, isLoading } = useQuery('super-heroes', fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 };
